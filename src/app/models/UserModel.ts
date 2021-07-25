160f1c12ae8a7f85b74db288c4ac1f375d3ad386
demianblood <jokervanya2@gmail.com>
2021-07-25 22:00:18 +0300

    перша частина лекції

---
+++ src/app/models/UserModel.ts
@@ -0,0 +1,17 @@
+// export class UserModel {
+//   name: string;
+//   age: number;
+//   status: boolean;
+//
+//
+//   constructor(name: string, age: number, status: boolean) {
+//     this.name = name;
+//     this.age = age;
+//     this.status = status;
+//   }
+// }
+export interface IUserModel {
+  name: string;
+  age: number;
+  status: boolean;
+}
