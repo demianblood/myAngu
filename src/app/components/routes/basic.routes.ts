3618762961841ed175dcd02eaeea505f1d06b0c4
demianblood <jokervanya2@gmail.com>
2021-08-03 19:36:50 +0300

    неготова 3 практична

--- src/app/components/routes/basic.routes.ts
+++ src/app/components/routes/basic.routes.ts
@@ -1,23 +1,19 @@
 import {Routes} from "@angular/router";
 import {UsersComponent} from "../users/users.component";
 import {UserDetailsComponent} from "../user-details/user-details.component";
-import {UserGuardService} from "../../services/user-guard.service";
-import {PostGuardService} from "../../services/post-guard.service";
 import {PostDetailsComponent} from "../post-details/post-details.component";
 import {PostsComponent} from "../posts/posts.component";
+
 
 export let routes: Routes =
   [
     {
       path: 'users',
       component: UsersComponent,
-      canDeactivate: [UserGuardService]
     },
     {
       path: 'users/:id',
       component: UserDetailsComponent,
-      canActivate: [UserGuardService],
-      canDeactivate: [UserGuardService]
     },
     {
       path: 'users',
@@ -27,13 +23,11 @@ export let routes: Routes =
     {
       path: 'posts',
       component: PostsComponent,
-      canDeactivate: [PostGuardService]
     },
     {
       path: 'posts/:id',
       component: PostDetailsComponent,
-      canActivate: [PostGuardService],
-      canDeactivate: [PostGuardService]
+
     },
     {
       path: 'posts',
